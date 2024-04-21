879767d6af6071f25e85670af9ffdee05ccac2f8
Jimly Nur Arif <132258178+jimlynurarif@users.noreply.github.com>
2024-04-21 09:20:07 +0700

    show link in startPage

--- src/backend/main.go
+++ src/backend/main.go
@@ -1,9 +1,63 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+    "fmt"
+    "net/http"
+	"strings"
+
+	"golang.org/x/net/html"
 )
+
+
+func getLinks(url string) []string {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return nil
+	}
+	defer resp.Body.Close()
+	links := []string{}
+	z := html.NewTokenizer(resp.Body)
+	for {
+		//contoh tt adalah EndTag atau bisa jadi StartTag
+		tt := z.Next()
+		switch tt {
+		case html.ErrorToken:
+			return links
+		case html.StartTagToken:
+			// contoh t adalah <a href="/wiki/Category:Go_(programming_language)" title="Category:Go (programming language)">
+			// atau <ul> atau <li> dan lain lain
+			t := z.Token()
+			// contoh t.Attr adalah [{ href //en.m.wikipedia.org/w/index.php?title=Go_(programming_language)&mobileaction=toggle_view_mobile} { class noprint stopMobileRedirectToggle}]
+			// atau 
+			// contoh t.Data adalah a  atau ul atau li atau div, dan lain lain
+			// buat boolean checkHref
+			havekHref := false
+			haveTitle := false
+			if t.Data == "a" {
+				for _, a := range t.Attr {
+					// fmt.Println(a)
+					// contoh a itu adalah { title AngularJS} atau { href /wiki/AngularJS} atau { href https://developer.wikimedia.org} atau dll
+					if a.Key == "href" {
+						if strings.HasPrefix(a.Val, "/wiki/") && !strings.Contains(a.Val, ":"){
+						havekHref = true
+						}
+					}
+					if a.Key == "title" {
+						// tidak boleh ada titik dua
+						str	:= a.Val
+						if !strings.Contains(str, ":"){
+							haveTitle = true
+						}
+					}
+				}
+				if havekHref && haveTitle {
+					links = append(links, t.Attr[0].Val)
+				}
+			}
+		}
+	}
+}
 
 func main() {
     // Membuat server untuk frontend
@@ -27,9 +81,16 @@ func main() {
 		// Debug
 		fmt.Printf("Start: %s, Finish: %s, Algorithm: %s\n", start, finish, algorithm)
 
+        if algorithm == "TES" {
+            start := "https://en.wikipedia.org/wiki/" + start
+            daftar:= getLinks(start)
+            fmt.Println(daftar)
+			fmt.Fprintf(w, "%v", daftar)
+        }
+
 		// Memproses data (ubah ini)
         w.WriteHeader(http.StatusOK)
-        w.Write([]byte("Data received successfully"))
+        // w.Write([]byte("Data received successfully"))
     })
 
     // Menyalakan server
