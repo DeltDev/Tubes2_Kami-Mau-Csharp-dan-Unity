0fff415a5cab60e3371e27ce77b3350a31a5f54d
Jimly Nur Arif <132258178+jimlynurarif@users.noreply.github.com>
2024-04-27 16:19:59 +0700

    fix: bfs

--- src/backend/main.go
+++ src/backend/main.go
@@ -10,7 +10,9 @@ import (
 	"sync"
 	"time"
 
-	"golang.org/x/net/html"
+	//"golang.org/x/net/html"
+	"github.com/gocolly/colly"
+	"github.com/gocolly/colly/queue"
 )
 
 type Response struct {
@@ -20,7 +22,242 @@ type Response struct {
 	Duration time.Duration
 }
 
+
+
+func removeRedundantMaphashtag(data map[string][]string) map[string][]string {
+	result := make(map[string][]string)
+
+	for key, arr := range data {
+		result[key] = removeRedundanthashtag(arr)
+	}
+
+	return result
+}
+
+func removeRedundanthashtag(array []string) []string {
+	result := make([]string, 0)
+	seen := make(map[string]int)
+
+	for _, word := range array {
+		parts := strings.Split(word, "#")
+		baseWord := parts[0]
+
+		if idx, ok := seen[baseWord]; ok {
+			if len(word) < len(result[idx]) {
+				result[idx] = word
+			}
+		} else {
+			seen[baseWord] = len(result)
+			result = append(result, word)
+		}
+	}
+
+	return result
+}
+
+func removeRedundantMap(data map[string][]string) map[string][]string {
+	result := make(map[string][]string)
+
+	for key, arr := range data {
+		result[key] = removeRedundant(arr)
+	}
+
+	return result
+}
+
+func removeRedundant(array []string) []string {
+	result := make([]string, 0)
+	seen := make(map[string]int)
+
+	for _, word := range array {
+		parts := strings.Split(word, "_")
+		baseWord := parts[0]
+
+		if idx, ok := seen[baseWord]; !ok || len(word) < len(array[idx]) {
+			seen[baseWord] = len(result)
+			result = append(result, word)
+		}
+	}
+
+	return result
+}
+
+
 func bfs(startPage string, endPage string) []string {
+
+ path := [][]string{{startPage}}
+ queue := []string{startPage}
+ visited := make(map[string]bool)
+ visited[startPage] = false
+ if startPage == endPage {
+  fmt.Println("Found the end page!")
+  fmt.Println("Path: ", startPage)
+  return []string{startPage}
+ }
+ var tempqueue []string
+ var wannaGetLinks []string
+
+ fmt.Println("flag 1")
+ for len(queue) >= 0 {
+        fmt.Println("flag 2")
+        if len(queue) == 0 {
+
+            if len(tempqueue) == 0 {
+                fmt.Println("queue dan tempqueue habis")
+                return []string{}
+            }
+
+            fmt.Println("queue habis")
+            queue = append(queue, tempqueue...)
+            tempqueue = []string{}
+            fmt.Println("panjang queue: ", len(queue))
+            fmt.Println("panjang tempqueue reset: ", len(tempqueue))
+        }
+
+        if len(queue) > 0 {
+            if len(queue) > 4000 {
+                wannaGetLinks = queue[:4000]
+            } else {
+                wannaGetLinks = queue
+            }
+            queue = queue[len(wannaGetLinks):]
+
+            // fmt.Println("getLink jalan=============================================")
+            parentAndChildMap := getLinks(wannaGetLinks)
+            fmt.Println("getLink selesai")
+            fmt.Println("panjang parentAndChildMap: ", len(parentAndChildMap))
+            fmt.Println("panjang queue: ", len(queue))
+            // fmt.Println("parentAndChildMap: Asli ****************************** ", parentAndChildMap)
+            parentAndChildMap = removeRedundantMap(parentAndChildMap)
+            parentAndChildMap = removeRedundantMaphashtag(parentAndChildMap)
+
+            for parent, arrChild := range parentAndChildMap{
+                //fmt.Println("parent: ", parent)
+                if parent == "Indonesia"{
+                    fmt.Println("================================")
+                }
+                for _, l := range arrChild {
+                    if !visited[l]{
+                        visited[l] = true
+                        tempqueue = append(tempqueue, l)
+                        foundParent := false
+                        i := 0
+                        for !foundParent {
+                            if path[i][len(path[i])-1] == parent {
+                                foundParent = true
+                                newPath := make([]string, len(path[i]))
+                                copy(newPath, path[i])
+                                newPath = append(newPath, l)
+                                path = append(path, newPath)
+                                if l == endPage {
+                                    fmt.Println("Found the end page!")
+                                    fmt.Println("Path: ", newPath)
+                                    return newPath
+                                }
+                            }
+                            i++
+                        }
+
+                    }
+
+                }
+            }
+        }
+ }
+ return []string{}
+}
+
+type SafeLinksMap struct {
+    sync.Map
+    mux sync.Mutex // Mutex for SafeLinksMap
+}
+
+func (s *SafeLinksMap) StoreLinks(key string, value []string) {
+    s.mux.Lock()
+    defer s.mux.Unlock()
+    s.Store(key, value)
+}
+
+func (s *SafeLinksMap) LoadLinks(key string) ([]string, bool) {
+    value, ok := s.Load(key)
+    if !ok {
+        return nil, false
+    }
+    return value.([]string), true
+}
+
+func getLinks(juduls []string) map[string][]string {
+    results := SafeLinksMap{}
+
+    c := colly.NewCollector(
+        colly.MaxDepth(1),
+    )
+    c.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: 4000})
+
+    isExist := SafeLinksMap{}
+
+    q, _ := queue.New(
+        22,
+        &queue.InMemoryQueueStorage{MaxSize: 1000000},
+    )
+
+    for _, judul := range juduls {
+        q.AddURL("https://en.wikipedia.org/wiki/" + judul)
+        results.StoreLinks(judul, make([]string, 0)) // Initialize with empty slice
+    }
+
+    c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+        link := e.Attr("href")
+        if strings.HasPrefix(link, "/wiki/") && !strings.Contains(link, ":") && link != "/wiki/Main_Page" {
+            if _, ok := isExist.Load(link); !ok {
+                isExist.Store(link, true)
+                link := strings.TrimPrefix(link, "/wiki/")
+                judul := e.Request.URL.String()
+                judul = strings.TrimPrefix(judul, "https://en.wikipedia.org/wiki/")
+                value, ok := results.LoadLinks(judul)
+                if ok {
+                    links := value
+                    if !contains(links, link) { // Check if link already exists
+                        links = append(links, link)
+                        results.StoreLinks(judul, links)
+                    }
+                }
+            }
+        }
+    })
+
+    c.OnRequest(func(r *colly.Request) {
+        // fmt.Println("visiting", r.URL)
+    })
+
+    err := q.Run(c)
+
+    if err != nil {
+        return nil
+    }
+
+    c.Wait()
+
+    hasil := make(map[string][]string)
+    results.Range(func(key, value interface{}) bool {
+        hasil[key.(string)] = append([]string{}, value.([]string)...)
+        return true
+    })
+
+    return hasil
+}
+
+// Helper function to check if a slice contains a string
+func contains(slice []string, str string) bool {
+    for _, s := range slice {
+        if s == str {
+            return true
+        }
+    }
+    return false
+}
+
+/*func bfs(startPage string, endPage string) []string {
 
 	path := [][]string{{startPage}}
 	queue := []string{startPage}
@@ -127,7 +364,7 @@ func getLinks(url string) []string {
 			}
 		}
 	}
-}
+}*/
 
 func main() {
 	// Membuat server untuk frontend
