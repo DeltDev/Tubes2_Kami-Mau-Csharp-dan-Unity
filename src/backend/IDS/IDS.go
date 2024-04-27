38ca41c35404a82d7db783ec9f90fdd0d484d43f
DeltDev <deltenc88@gmail.com>
2024-04-27 22:53:08 +0700

    chore: IDS.go comments

--- src/backend/IDS/IDS.go
+++ src/backend/IDS/IDS.go
@@ -16,21 +16,21 @@ func IDS(startPage string, endPage string) ([]string,int) {
 	path := []string{}
 	temp:= 0
 	total := 0
-	for iteration := 1; iteration <= 8; iteration += 2 { //iterasi fragmentasi IDS multithreading (fragmentasi: IDS dipecah menjadi asumsi 1-9 degree)
+	for iteration := 1; iteration <= 8; iteration += 2 { //iterasi fragmentasi IDS multithreading (fragmentasi: IDS dipecah menjadi asumsi 1-8 degree dengan membangkitkan 2 buah thread secara bersamaan)
 		fmt.Println("Fragmen dari: ",iteration," sampai: ",iteration+1)
-		path,temp= IDSFragment(startPage, endPage, iteration, iteration+1)
-		total += temp
+		path,temp= IDSFragment(startPage, endPage, iteration, iteration+1) //cari path dari kedalaman iteration sampai iteration+1 secara bersamaan dengan IDS terfragmentasi
+		total += temp //jumlahkan link yang dikunjungi
 		if path != nil { //kalau sudah ketemu path dari fragmen, jangan dilanjutkan IDSnya
 			break
 		}
 	}
 
-	return path,total
+	return path,total //kembalikan path dan total link yang dikunjungi
 }
 
 func DLS(src string, target string, limit int, visited map[string]bool, stopExplore chan bool, visCount* int) ([]string, bool) {
 	visited[src] = true //tandai sudah pernah divisit
-	*visCount++
+	*visCount++ //tambah jumlah halaman yang dikunjungi dengan 1 (halaman saat ini)
 	if src == target { //kalau halaman yang divisit sekarang sama dengan halaman yang dicari
 		ret := []string{src} //masukin ke path
 		return ret, true    //artinya sudah ketemu pathnya
@@ -41,34 +41,32 @@ func DLS(src string, target string, limit int, visited map[string]bool, stopExpl
 	}
 
 	links := scraper.GetLinksArr(src) //dapatkan semua link yang ada di halaman yang sedang dikunjungi
-	// links = scraper.RemoveRedundant(links) //hilangkan semua link yang redundant
-	// links = scraper.RemoveRedundanthashtag(links) //hilangkan semua link yang mengandung # (karena hanya merupakan redirect ke halaman yang sama)
 	links = scraper.SortStringsBySim(target,links) //urutkan semua link berdasarkan kemiripan dengan target
-	fmt.Println(target)
+	fmt.Println(target) //debug
 	fmt.Println(links)
 	for _, nextLink := range links {         //iterasi ke semua link yang ada di halaman yang sedang dikunjungi
-		if visited[nextLink] {
+		if visited[nextLink] { //lewati link yang sudah dikunjungi
 			continue
 		}
-		visited[nextLink] = true
+		visited[nextLink] = true //tandai link selanjutnya dengan true
 		select {
-		case explored := <-stopExplore:
-			if explored {
-				return nil, true
+		case explored := <-stopExplore: //jika ada sinyal untuk menghentikan algoritma IDS
+			if explored { 
+				return nil, true //hentikan semua algoritma IDS
 			}
 		default:
 			subPath, found := DLS(nextLink, target, limit-1, visited, stopExplore, visCount) //kunjungi node selanjutnya dan kurangi limit dengan 1 dan dapatkan nilai subpath dan nilai sudah ketemu path atau belum
-			if found {                                                             //kalau ketemu
+			if found { //kalau ketemu
 				return append([]string{src}, subPath...), true //tambahkan nama halaman yang sedang dikunjungi sekarang ke subpath dan tandai pathnya ketemu
 			}
 		}
 
-		delete(visited, nextLink)
+		delete(visited, nextLink) //Backtrack visited
 	}
 	return nil, false //tidak ketemu pathnya
 }
 
-func IDSFragment(startPage string, endPage string, startIdx int, endIdx int) ([]string,int) {
+func IDSFragment(startPage string, endPage string, startIdx int, endIdx int) ([]string,int) { //thread IDS terfragmentasi
 	var wg sync.WaitGroup
 	wg.Add(2)
 
@@ -76,15 +74,15 @@ func IDSFragment(startPage string, endPage string, startIdx int, endIdx int) ([]
 	stopExplore := make(chan bool, 1)
 	total:=0
 	
-	for iteration := startIdx; iteration <= endIdx; iteration++ { //tambah kedalaman terus sampai ketemu pathnya
+	for iteration := startIdx; iteration <= endIdx; iteration++ { //tambah kedalaman terus dari startIDX sampai endIdx sampai ketemu pathnya
 		initVal :=0
-		go func(d int) {
+		go func(d int) { //buat goroutine
 			defer wg.Done()
-			path, found:= DLS(startPage, endPage, d, map[string]bool{}, stopExplore, &initVal)
-			if found {
-				ch <- path
-				stopExplore <- true
-				total += initVal
+			path, found:= DLS(startPage, endPage, d, map[string]bool{}, stopExplore, &initVal) //dapatkan path dan apakah berhasil ditemukan
+			if found { // jika ditemukan
+				ch <- path //masukkan path ke channel
+				stopExplore <- true //hentikan semua goroutine DLS
+				total += initVal //jumlahkan banyak link yang divisit
 				return
 			}
 			ch <- nil
@@ -96,11 +94,11 @@ func IDSFragment(startPage string, endPage string, startIdx int, endIdx int) ([]
 	for i := 0; i < 2; i++ {
 		path := <-ch
 
-		if path != nil {
-			if path[0] == startPage && path[len(path)-1] == endPage {
-				return path, total
+		if path != nil { //jika path tidak kosong
+			if path[0] == startPage && path[len(path)-1] == endPage { //periksa jika link pertama adalah startPage dan link terakhir adalah endPage
+				return path, total //return path yang udah ketemu dan banyak link total yang dikunjungi
 			}
 		}
 	}
-	return nil,total //return path yang udah ketemu
+	return nil,total //return path yang tidak ditemukan
 }
